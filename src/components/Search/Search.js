dc986e7a93841a25e5a31cc4f21a8a1c69e87e86
Jarvis Tse <jarvis.tse.developer@gmail.com>
2023-06-07 19:05:42 -0400

    update search bar ui

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,16 +1,20 @@
 import "./Search.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const Search = ({handleSubmit, handleCityChange, city}) => {
     return (
-        <form onSubmit={handleSubmit}>
+        <form className="inputForm" onSubmit={handleSubmit}>
             <input
                 className="inputBox"
                 type="text"
                 value={city}
                 onChange={handleCityChange}
                 placeholder="City"
-                />
-            <button type="submit">Submit</button>
+            />
+            <button className="submitBtn" type="submit">
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
         </form>
     );
 }
