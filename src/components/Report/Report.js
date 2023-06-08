d5daef1bff7746661a145c33a0d28e5a71df8d37
Jarvis Tse <jarvis.tse.developer@gmail.com>
2023-06-07 21:36:23 -0400

    add min and max temp in report

--- src/components/Report/Report.js
+++ src/components/Report/Report.js
@@ -1,4 +1,6 @@
 import "./Report.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons";
 
 const Report = ({weather}) => {
     return (
@@ -13,9 +15,20 @@ const Report = ({weather}) => {
                         <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description}></img>
                         <span className="tempText">{Math.round(weather.main.temp)}°C</span>
                     </div>
-                    <div className="weatherDescription">{weather.weather[0].description}</div>
+                    <div className="tempRange">
+                        <div className="tempRangeFlex">
+                            <span className="tempMaxIcon"><FontAwesomeIcon icon={faArrowUp} /></span>
+                            <span>{Math.round(weather.main.temp_max)}°C</span>
+                        </div>
+                        <div className="tempRangeFlex">
+                            <span className="tempMinIcon"><FontAwesomeIcon icon={faArrowDown} /></span>
+                            <span>{Math.round(weather.main.temp_min)}°C</span>
+                        </div>
+                    </div>
                 </div>
+                
                 <div className="weatherDetails">
+                    <h3>{weather.weather[0].description}</h3>
                     <h3>Feels like {Math.round(weather.main.feels_like)}°C</h3>
                     <div className="weatherStat">
                         <div className="weatherStatFlex">
